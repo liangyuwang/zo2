5fa471576c7dfa1e92e0004e4f29d2c1ac4dd710
liangyuwang <ly.wang.2022@outlook.com>
2025-02-02 16:02:56 +0300

    fix bug of BaseZOModel

--- zo2/model/base.py
+++ zo2/model/base.py
@@ -5,29 +5,16 @@ class BaseZOModel(torch.nn.Module):
         super().__init__()
         self.zo_training = True
 
-    def train(self, mode: bool=True):
-        """
-            First-order training
-        """
-        self.zo_training = False
-        super().train(mode)
-
-    def eval(self):
-        """
-            First-order evaluation
-        """
-        self.zo_training = False
-        super().eval()
-
     def zo_train(self):
         """
             Zeroth-order training
         """
         self.zo_training = True
+        self.eval()
 
     def zo_eval(self):
         """
             Zeroth-order evaluation
         """
         self.zo_training = False
-        super().eval()
+        self.eval()
