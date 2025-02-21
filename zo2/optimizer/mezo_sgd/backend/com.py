588e869a38c93cca859665da24b121624836e0c1
liangyuwang <ly.wang.2022@outlook.com>
2025-02-21 03:29:48 +0300

    fix bugs

--- zo2/optimizer/mezo_sgd/backend/com.py
+++ zo2/optimizer/mezo_sgd/backend/com.py
@@ -4,13 +4,15 @@ def compute_module_impl(
         module_dual_forward,
         module: torch.nn.Module,
         optimize_method: str,
-        *args, **kwargs
+        *args, 
+        optimize_kwargs = None,
+        **kwargs
     ):
     match optimize_method:
         case "":
             pass
         case "torch.compile":   # may introduce some precision mismatch
-            module = torch.compile(module, *args, **kwargs)
+            module = torch.compile(module, **optimize_kwargs)
         case _:
             raise NotImplementedError
     return module_dual_forward(module=module, *args, **kwargs)
@@ -19,13 +21,15 @@ def compute_function_impl(
         function_dual_forward,
         fn,
         optimize_method: str,
-        *args, **kwargs
+        *args, 
+        optimize_kwargs = None,
+        **kwargs
     ):
     match optimize_method:
         case "":
             pass
         case "torch.jit.script":   # may introduce some precision mismatch
-            fn = torch.jit.script(fn, *args, **kwargs)
+            fn = torch.jit.script(fn, **optimize_kwargs)
         case _:
             raise NotImplementedError
     return function_dual_forward(fn, *args, **kwargs)
