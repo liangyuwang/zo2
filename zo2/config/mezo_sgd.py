5ecfff6da6f71c20c20d455197e7559979e02994
liangyuwang <ly.wang.2022@outlook.com>
2025-02-21 03:09:58 +0300

    add more zo2 configs

--- zo2/config/mezo_sgd.py
+++ zo2/config/mezo_sgd.py
@@ -15,9 +15,12 @@ class MeZOSGDConfig:
     # zo2 config
     zo2: bool = True    # use offloading or not
     offloading_blocks: list = None  # specify offloading blocks or not
-    offloading_device: str = 'cpu'  # offload device
-    working_device: str = 'cuda'    # compute device
+    offloading_device: str = 'cpu'  # offload device, can be CPU or a file path (disk offloading)
+    working_device: str = 'cuda'    # compute device, can be any CUDA device
     overlap: bool = True    # use scheduler to overlap or not
+    compute_module_optimize_method: str = ''   # '', 'torch.compile'
+    compute_function_optimize_method: str = ''   # '', 'torch.jit.script'
+    communicate_optimize_method: str = ''   # '', 'bucket'
     amp: bool = False   # use amp or not
     precision_on_offloading_device: torch.dtype = torch.float16 # precision on offloading device, valid when using amp
     precision_on_working_device: torch.dtype = torch.float32    # precision on working device, valid when using amp
